83d283ef79daae8a456de3ed924133e37fe5d986
Naillton <nailton_junior@protonmail.com>
2024-02-01 16:03:33 -0300

    Setting Dependency Injection Net Module Retrofit

--- app/src/main/java/com/nailton/managerpassword/presentation/dependencyinjection/modules/NetModule.kt
+++ app/src/main/java/com/nailton/managerpassword/presentation/dependencyinjection/modules/NetModule.kt
@@ -1,8 +1,11 @@
 package com.nailton.managerpassword.presentation.dependencyinjection.modules
 
+import com.google.gson.GsonBuilder
+import com.nailton.managerpassword.data.API.MPService
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -10,8 +13,18 @@ class NetModule(
     private val baseurl: String
 ) {
 
-    //@Singleton
-    //@Provides
-    //fun providesRetrofit(): Retrofit {
-    //}
+    @Singleton
+    @Provides
+    fun providesRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .baseUrl(baseurl)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMPService(retrofit: Retrofit): MPService {
+        return retrofit.create(MPService::class.java)
+    }
 }
