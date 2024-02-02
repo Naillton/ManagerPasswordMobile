0b949343f897463394baeda84e4f3f779d071bfc
Naillton <nailton_junior@protonmail.com>
2024-02-02 15:29:45 -0300

    Remove variable in build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-        apikey = ""
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
